48ff69d2f0ba407bfca49545d70fbf8101111ab3
KunalKhobragadek <95751647+KunalKhobragadek@users.noreply.github.com>
2022-01-03 13:58:21 +0530

    Add files via upload

---
+++ Area of rectangle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float L,B,Area;
+    printf("\n Enter the Lenth and Breadth of Recangle :");
+    scanf("%f%f",&L,&B);
+     Area = L*B;
+    printf("\n The Area is = %f", Area);
+    return 0;
+}
